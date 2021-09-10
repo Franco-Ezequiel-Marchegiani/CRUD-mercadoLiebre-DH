fd057cff26c0243e8e5a46634bf4faeba38e5322
Franco Ezequiel <eze.cavs@hotmail.com>
2021-09-10 19:55:36 -0300

    Subo pequeños cambios realizados

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,70 +1,104 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	// Root - Show all products
-	index: (req, res) => {
-		// Do the magic
-		res.render("products", { products});
-	},
+  // Root - Show all products
+  index: (req, res) => {
+    // Do the magic
+    res.render("products", { products });
+  },
 
-	// Detail - Detail from one product
-	detail: (req, res) => {
-		// Do the magic
-		 let productoDetalle = products.find( product =>{
-			return product.id == req.params.id
-		})
-		res.render("detail", { productoDetalle: productoDetalle}); 
-		//res.send("Estás en detalle producto de:" + req.params.id)
-	},
+  // Detail - Detail from one product
+  detail: (req, res) => {
+    // Do the magic
+    let productoDetalle = products.find((product) => {
+      return product.id == req.params.id;
+    });
+    res.render("detail", { productoDetalle: productoDetalle });
+    //res.send("Estás en detalle producto de:" + req.params.id)
+  },
 
-	// Create - Form to create
-	create: (req, res) => {
-		// Do the magic
-		res.render("product-create-form");
-	},
-	
-	// Create -  Method to store
-	store: (req, res) => {
-		// Do the magic
-		res.send(req.body)
-		res.redirect('/products/')
-	},
+  // Create - Form to create
+  create: (req, res) => {
+    // Do the magic
+    res.render("product-create-form");
+  },
 
-	// Update - Form to edit
-	edit: (req, res) => {
-		// Do the magic
-		let productoEditar = products.find(product =>{
-			return product.id == req.params.id
-		})
-		res.render("product-edit-form", {productoEditar});
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		// Do the magic
-		let peliculaEncontrada = products.find(pelicula =>{
-			return pelicula.id == req.params.id
-		});
-		res.redirect('/products/')
-		res.render(
-			peliculaEncontrada.name = req.body.name,
-			peliculaEncontrada.price = req.body.price,
-			peliculaEncontrada.discount = req.body.discount,
-			peliculaEncontrada.category = req.body.category,
-			peliculaEncontrada.description = req.body.description
-		)
-	},
+  // Create -  Method to store
+  store: (req, res) => {
+    // Do the magic
+    const lastProduct = products[products.length - 1];
+    const biggestProductId = products.length > 0 ? lastProduct.id : 1;
+    if (!req.file) {
+      res.redirect("/products/create");
+    }
+    const product = {
+      ...req.body,
+      id: biggestProductId + 1,
+      price: Number(req.body.price),
+      discount: Number(req.body.discount), //Se especifica que sea tipo de dato Number, caso contrario lo convierte en String
+      image: req.file.filename,
+      /* ...req.body 
+			Es lo mismo que esto: 
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+			(También funciona con los ID's)
+			*/
+    };
+    products.push(product);
+    const jsonString = JSON.stringify(products, null, 4); //El null es para saltar una función, y el 4 son los espacios que genera entre producto y producto
+    fs.writeFileSync(productsFilePath, jsonString);
+    res.redirect("/products/");
+  },
 
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		// Do the magic
-		res.send("Estás en: destroy" +req.params.id)
-	}
+  // Update - Form to edit
+  edit: (req, res) => {
+    // Do the magic
+    let productoEditar = products.find((product) => {
+      return product.id == req.params.id;
+    });
+    res.render("product-edit-form", { productoEditar });
+  },
+  // Update - Method to update
+  update: (req, res) => {
+    // Do the magic
+    let peliculaEncontrada = products.find((pelicula) => {
+      return pelicula.id == req.params.id;
+    });
+
+    peliculaEncontrada.name = req.body.name;
+    peliculaEncontrada.price = Number(req.body.price);
+    peliculaEncontrada.discount = Number(req.body.discount);
+    peliculaEncontrada.category = req.body.category;
+    peliculaEncontrada.description = req.body.description;
+    peliculaEncontrada.image = req.file
+      ? req.file.filename
+      : peliculaEncontrada.image;
+
+    const jsonString = JSON.stringify(products, null, 4);
+    fs.writeFileSync(productsFilePath, jsonString);
+    res.redirect("/products/");
+  },
+
+  // Delete - Delete one product from DB
+  destroy: (req, res) => {
+    // Do the magic
+    products = products.filter((product) => {
+      return product.id != req.params.id;
+    });
+    res.render(products);
+    const jsonString = JSON.stringify(products, null, 4);
+    fs.writeFileSync(productsFilePath, jsonString);
+    res.redirect("/products");
+  },
 };
 
 module.exports = controller;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,15 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer');
+const path = require('path')
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, "../../public/images/products"),
+    filename: (req, file, callback) =>{
+        callback(null, Date.now() + path.extname(file.originalname));
+    }
+})
+const upLoader = multer({storage})
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
@@ -10,7 +18,7 @@ router.get('/', productsController.index);
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create', productsController.create); 
-router.post('/', productsController.store); 
+router.post('/', upLoader.single("image"),productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
